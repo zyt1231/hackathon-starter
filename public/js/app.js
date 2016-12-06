3390b2779f4075b97e22179ea2bc4015c96ae741
Yuting Zhou <zyt1231@gmail.com>
2016-12-06 14:13:49 -0500

    add item to cart

--- public/js/app.js
+++ public/js/app.js
@@ -3,5 +3,20 @@
  */
 var app = angular.module('product', []);
 app.controller('productCtrl', function ($scope) {
-    $scope.name = "John Doe";
-});
+        $scope.items = local_data;
+        $scope.cartItems = [];
+        $scope.totalPrice = 0;
+        $scope.addItem = function (item) {
+            $scope.cartItems.push(item);
+            console.log("Cart Items:");
+            console.log($scope.cartItems);
+            console.log($scope.cartItems.length);
+            console.log($scope.totalPrice);
+            var total = $scope.totalPrice;
+            $scope.cartItems.forEach(function(item){
+                total += item.price;
+            });
+            $scope.totalPrice = total;
+        };
+    }
+);
