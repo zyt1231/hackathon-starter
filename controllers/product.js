355b464d69b0f878cbdd4473bcefa0de6d5843bc
Yuting Zhou <zyt1231@gmail.com>
2016-12-18 14:14:31 -0500

    add filter

--- controllers/product.js
+++ controllers/product.js
@@ -8,27 +8,22 @@
 const Product = require('../models/Product');
 exports.getProducts = (req, res) => {
 
-    var p1 = new Product({
-        sku:'456333',
-        title:'Circle Logo Pendant',
-        description:'Brilliant-cut crystal in a thick, MK logo-engraved bezel pendant setting.',
-        brand:'G',
-        category:'necklace',
-        quantity:2,
-        price:44,
-        img:'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQK0gy5CKFxeMygabp3mthESQecfPIVIu9Bw-rPq0-KVdVHnDlXxLIP8V02YF5C0CjUdAdDm2E&usqp=CAY'
-    });
-    p1.save();
-
-
+    // var p1 = new Product({
+    //     sku:'456333',
+    //     title:'Circle Logo Pendant',
+    //     description:'Brilliant-cut crystal in a thick, MK logo-engraved bezel pendant setting.',
+    //     brand:'G',
+    //     category:'necklace',
+    //     quantity:2,
+    //     price:44,
+    //     img:'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQK0gy5CKFxeMygabp3mthESQecfPIVIu9Bw-rPq0-KVdVHnDlXxLIP8V02YF5C0CjUdAdDm2E&usqp=CAY'
+    // });
+    // p1.save();
 
 
     Product.find({}, function(err,products){
         var prodList = [];
-        // products.forEach(function(product){
-        //     prodList.append(product);
-        // });
-        console.log(products);
+        // console.log(products);
         res.render('product', {
             title: 'Product',
             data:   products
@@ -38,19 +33,5 @@ exports.getProducts = (req, res) => {
     });
 
 
-
-
-
-    // res.render('product', {
-    //     title: 'Product',
-    //     data:'testing'
-    // });
 };
-// exports.getLogin = (req, res) => {
-//     if (req.user) {
-//         return res.redirect('/');
-//     }
-//     res.render('account/login', {
-//         title: 'Login'
-//     });
-// };
+
--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,31 @@
  * Created by Ting on 11/27/16.
  */
 var app = angular.module('product', []);
-app.controller('productCtrl', function ($scope) {
+app.controller('productCtrl', ['$scope',function ($scope) {
+        $scope.brandModel = {
+            LiaSophia: true,
+            LuckyBrand: true,
+            KateSpade: true,
+            Brighton: true,
+            handmade: true,
+            unbranded:true
+        };
+        $scope.categoryModel = {
+            necklace:true,
+            earrings:true,
+            ring:true,
+            name:''
+        };
         $scope.items = local_data;
+
+        $scope.brandModel = {
+            LiaSophia: true,
+            LuckyBrand: false,
+            KateSpade: false,
+            Brighton: false,
+            handmade: false,
+            unbranded:false
+        };
         $scope.cartItems = [];
         $scope.totalPrice = 0;
         $scope.addItem = function (item) {
@@ -13,10 +36,19 @@ app.controller('productCtrl', function ($scope) {
             console.log($scope.cartItems.length);
             console.log($scope.totalPrice);
             var total = $scope.totalPrice;
-            $scope.cartItems.forEach(function(item){
+            $scope.cartItems.forEach(function (item) {
                 total += item.price;
             });
             $scope.totalPrice = total;
         };
-    }
-);
+        $scope.filterByProperties = function filterByProperties(item) {
+            var category = item.category;
+            var brand = item.brand;
+            return $scope.categoryModel.name==category & $scope.brandModel[brand];
+
+
+
+            // return true;
+        };
+
+    }]);
